157472f268701d40ce77c26d76fccc1da3450402
Connor Yates <connoryates@Connors-MacBook-Pro.local>
2021-07-23 13:38:37 -0500

    navbar layout

---
+++ restaurant/src/Components/Navigation.js
@@ -0,0 +1,27 @@
+
+
+export default (params) => {
+   return(
+  <nav className=" p-6">
+  <div className="lg:w-auto">
+    <div className="text-sm lg:flex-grow">
+      <a href className="hover:text-black mr-4">
+       Welcome
+      </a>
+      <a href className=" hover:text-black mr-4">
+        About us
+      </a>
+      <a href className=" hover:text-black mr-4">
+        Menu
+      </a>
+      <a href className=" hover:text-black mr-4">
+       Gallery
+      </a>
+      <a href className="hover:text-black mr-4">
+        News
+      </a>
+    </div>
+  </div>
+</nav>
+   )
+}
--- restaurant/src/Components/index.js
+++ restaurant/src/Components/index.js
@@ -3,3 +3,4 @@ export {default as Signature} from './Signature'
 export {default as LunchSets} from './LunchSets'
 export {default as Dinner} from './Dinner'
 export {default as Header} from "./Header"
+export {default as Navigation} from "./Navigation"
