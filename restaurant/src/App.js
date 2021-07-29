81be780ef42e50308107604530566a906ad436c9
Connor <bonnor@Bonnors-MacBook-Air.local>
2021-07-29 10:23:33 -0700

    imagegallery

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Header />
       <About/>
       <Menu />
-      {/* <ImageGallery/> */}
+      <ImageGallery/>
     </Fragment>
   );
 }
--- restaurant/src/Components/ImageGallery.js
+++ restaurant/src/Components/ImageGallery.js
@@ -1,21 +1,23 @@
-// import BigOlChicken from "../images/BigOlChicken.jpg"
-// import dumplings from "../images/dumplings.jpg"
-// import SeafoodGumbo from "../images/SeafoodGumbo.jpg"
-// import appetizers from "../images/appetizers.jpeg"
-// import BarTop from "../images/BarTop.jpg"
-// // import GrilledChicken from "../images/GrilledChiken.jpg"
-// import Noodles from "../images/Noodles.jpg"
-// import Shakshuka from "../images/Shakshouka.jpg"
+import BigOlChicken from "../images/BigOlChicken.jpg"
+import dumplings from "../images/dumplings.jpg"
+import SeafoodGumbo from "../images/SeafoodGumbo.jpg"
+import appetizers from "../images/smallerAppetizers.jpeg"
+import BarTop from "../images/BarTop.jpg"
+import GrilledChicken from "../images/GrilledChicken.jpg"
+import Noodles from "../images/Noodles.jpg"
+import Shakshuka from "../images/Shakshouka.jpg"
 
-// export default (params) => {
-//     <div className="flex-wrap">
-//         <img src={BigOlChicken} alt="chicken" />
-//         <img src={SeafoodGumbo} alt="gumbo" />
-//         <img src={appetizers} alt="appetizers" />
-//         <img src={dumplings} alt="dumplings" />
-//         <img src={BarTop} alt="bartop" />
-//         {/* <img src={GrilledChicken} alt="grilledchicken" /> */}
-//         <img src={Noodles} alt="noodles" />
-//         <img src={Shakshuka} alt="shakshouka" />
-//     </div>
-// }
+export default (params) => {
+    return(
+    <div className=" grid grid-rows-2 grid-flow-col ">
+        <img src={BigOlChicken} alt="chicken" />
+        <img src={SeafoodGumbo} alt="gumbo" />
+        <img src={appetizers} alt="appetizers" />
+        <img src={dumplings} alt="dumplings" />
+        <img src={BarTop} alt="bartop" />
+        <img src={GrilledChicken} alt="grilledchicken" />
+        <img src={Noodles} alt="noodles" />
+        <img src={Shakshuka} alt="shakshouka" />
+    </div>
+    )
+}
