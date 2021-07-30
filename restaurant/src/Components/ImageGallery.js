ceaa2e77ce72c92a0ce0a09f9c2ca990cde12490
AustinSkrobar <austin.skrobar@gmail.com>
2021-07-30 13:47:02 -0500

    made sure everything was formated and in the right order

--- restaurant/src/Components/ImageGallery.js
+++ restaurant/src/Components/ImageGallery.js
@@ -1,23 +1,23 @@
-import BigOlChicken from "../images/BigOlChicken.jpg"
-import dumplings from "../images/dumplings.jpg"
-import SeafoodGumbo from "../images/SeafoodGumbo.jpg"
-import appetizers from "../images/smallerAppetizers.jpeg"
-import BarTop from "../images/BarTop.jpg"
-import GrilledChicken from "../images/GrilledChicken.jpg"
-import Noodles from "../images/Noodles.jpg"
-import Shakshuka from "../images/Shakshouka.jpg"
+import BigOlChicken from "../images/BigOlChicken.jpg";
+import dumplings from "../images/dumplings.jpg";
+import SeafoodGumbo from "../images/SeafoodGumbo.jpg";
+import appetizers from "../images/smallerAppetizers.jpeg";
+import BarTop from "../images/BarTop.jpg";
+import GrilledChicken from "../images/GrilledChicken.jpg";
+import Noodles from "../images/Noodles.jpg";
+import Shakshuka from "../images/Shakshouka.jpg";
 
 export default (params) => {
-    return(
+  return (
     <div className=" grid grid-rows-2 grid-flow-col ">
-        <img src={BigOlChicken} alt="chicken" />
-        <img src={SeafoodGumbo} alt="gumbo" />
-        <img src={appetizers} alt="appetizers" />
-        <img src={dumplings} alt="dumplings" />
-        <img src={BarTop} alt="bartop" />
-        <img src={GrilledChicken} alt="grilledchicken" />
-        <img src={Noodles} alt="noodles" />
-        <img src={Shakshuka} alt="shakshouka" />
+      <img src={BigOlChicken} alt="chicken" />
+      <img src={SeafoodGumbo} alt="gumbo" />
+      <img src={appetizers} alt="appetizers" />
+      <img src={dumplings} alt="dumplings" />
+      <img src={BarTop} alt="bartop" />
+      <img src={GrilledChicken} alt="grilledchicken" />
+      <img src={Noodles} alt="noodles" />
+      <img src={Shakshuka} alt="shakshouka" />
     </div>
-    )
-}
+  );
+};
--- restaurant/src/Components/Map.js
+++ restaurant/src/Components/Map.js
@@ -24,7 +24,7 @@ export default (params) => {
   };
 
   return (
-    <div>
+    <div className="py-10">
       <div className="flex flex-wrap justify-center">
         <div className="w-2/5 h-96 mb-5 bg-gray-200">
           <h1>Map Placeholder</h1>
@@ -52,7 +52,13 @@ export default (params) => {
               /> 
             </svg>*/}
             <label className="border p-1 w-44">
-              <input type="date" data-date-inline-picker="true" placeholder="Date" name="date" onChange={handleChange} />
+              <input
+                type="date"
+                data-date-inline-picker="true"
+                placeholder="Date"
+                name="date"
+                onChange={handleChange}
+              />
             </label>
           </div>
           <div className="flex flex-row p-5">
@@ -70,35 +76,42 @@ export default (params) => {
                 d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg> */}
-            
-              <input type="time" placeholder="Time" name="time" className="border p-1 w-44" onChange={handleChange} />
-            
+
+            <input
+              type="time"
+              placeholder="Time"
+              name="time"
+              className="border p-1 w-44"
+              onChange={handleChange}
+            />
           </div>
           <div className="p-5 flex flex-row w-44">
-            
             <label className="border p-1 flex flex-row">
-            
-              <input type="number" placeholder="Seats" name="people" onChange={handleChange} />
-              <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 m-2"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                clipRule="evenodd"
+              <input
+                type="number"
+                placeholder="Seats"
+                name="people"
+                onChange={handleChange}
               />
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 m-2"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                  clipRule="evenodd"
+                />
+              </svg>
             </label>
-            
           </div>
           <input
-              className="col-start-2 col-end-3 row-start-2 row-end-2 my-7 text-xs bg-black text-white"
-              type="submit"
-              value="Find A Table"
-            />
+            className="col-start-2 col-end-3 row-start-2 row-end-2 my-7 text-xs bg-black text-white"
+            type="submit"
+            value="Find A Table"
+          />
         </form>
       </div>
     </div>
