665120c64e16020b3c40537f307e01a197de6ef5
Connor Yates <connoryates@Connors-MacBook-Pro.local>
2021-08-05 12:53:13 -0500

    changes to footer

--- restaurant/src/Components/Footer.js
+++ restaurant/src/Components/Footer.js
@@ -1,10 +1,15 @@
 import Carrots from "../images/Carrots.jpg"
+import ZuchiniNoodles from "../images/ZuchiniNoodles.jpg"
+import Crossants from "../images/Croissants.jpg"
+import brightBerries from "../images/brightBerries.jpg"
+import darkBerries from "../images/darkBerries.jpg"
+import lettuceWraps from "../images/lettuceWraps.jpg"
 export default (params) => {
   return (
     <div>
-      <div className="w-full h-96 flex justify-center bg-footerGray">
+      <div className="w-full h-72 grid justify-items-center bg-footerGray">
         <div className="w-xl flex flex-row p-4">
-          <div className="w-1/3 text-white p-4">
+          <div className="ml-16 w-1/3 text-white p-4">
             <h1 className="text-xl">Restaurant</h1>
             <h4 className="text-xs">A PLACE TO EAT</h4>
             <p className="text-sm w-3/4 py-2 leading-loose">
@@ -13,8 +18,18 @@ export default (params) => {
             </p>
             
           </div>
-          <div className="w-1/3 text-white text-xl p-4">Gallery</div>
-          <div className="w-1/3 p-4">
+          
+          <div className="text-white text-xl p-4">Gallery
+          <div className=" grid grid-rows-2 grid-flow-col gap-0 ">
+          <img src={Carrots} alt=""/>
+          <img src={ZuchiniNoodles} alt=""/>
+          <img src={Crossants} alt=""/>
+          <img src={brightBerries} alt=""/>
+          <img src={darkBerries} alt=""/>
+          <img src={lettuceWraps} alt=""/>
+          </div>
+          </div>
+          <div className="ml-32 p-4">
             <h1 className="text-xl text-white">CONTACT INFO</h1>
             <p className="pt-2 text-sm text-gray-500 leading-loose">1402 7th St., El Paso, Texas </p>
             <p className="text-sm text-white leading-loose">(915) 334-9427</p>
