fe8c66d500b9022b5c1162d7cbabbe5fd39fab47
AustinSkrobar <austin.skrobar@gmail.com>
2021-08-11 13:07:57 -0500

    fixed reservation button at the top mostly

--- restaurant/src/Components/Booking.js
+++ restaurant/src/Components/Booking.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-
 export default (params) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [inputData, setInputData] = useState({
@@ -38,16 +37,31 @@ export default (params) => {
   };
 
   return (
-    <div>
-      <button className={`${modalIsOpen && "opacity-0"}`} onClick={setModalIsOpenToTrue}>Reservations</button>
-      <div className={`${modalIsOpen && "relative z-20 bg-black h-screen w-screen opacity-50"}`}>
-              {modalIsOpen ? (<div className="backdrop-opacity-100">
-              <button className="text-white" onClick={setModalIsOpenToFalse}>x</button>
-              <div>
-              <h1>Table</h1>
-              <h1>BOOKING</h1>
+    <div className="">
+      <button
+        onClick={setModalIsOpenToTrue}
+      >
+        Reservations
+      </button>
+
+      <div
+        className={`${
+          modalIsOpen &&
+          "w-1/3 fixed z-50 transform translate-x-full"
+        }`}
+      >
+        {modalIsOpen ? (
+          <div className="w-xl">
+            <button className="text-white" onClick={setModalIsOpenToFalse}>
+              x
+            </button>
+            <div className="flex flex-col border">
+              <p className="flex justify-center font-cursiveText text-4xl text-white italic">
+                Chef
+              </p>
+              <p className="flex justify-center text-5xl font-thin text-white">MENU</p>
               <form
-                className=" grid grid-cols-3 grid-rows-4"
+                className="grid grid-cols-3 grid-rows-4"
                 onSubmit={handleSubmit}
               >
                 <div className="p-5 flex flex-col w-44">
@@ -69,6 +83,9 @@ export default (params) => {
                   </label>
                 </div>
                 <div className="flex flex-col p-5 w-44">
+                <p className=" text-white">
+                    DATE
+                  </p>
                   <label className="border p-1 w-44">
                     <input
                       type="date"
@@ -80,21 +97,26 @@ export default (params) => {
                   </label>
                 </div>
                 <div className="flex flex-col p-5">
-                <label className="border p-1 flex flex-row w-44">
-                  <input
-                    type="time"
-                    placeholder="Time"
-                    name="time"
-                    className="  w-44"
-                    onChange={handleChange}
-                  />
+                <p className=" text-white">
+                    TIME
+                  </p>
+                  <label className="border p-1 flex flex-row w-44">
+                    <input
+                      type="time"
+                      placeholder="Time"
+                      name="time"
+                      className="  w-44"
+                      onChange={handleChange}
+                    />
                   </label>
                 </div>
                 <div className="p-5 flex flex-col ">
+                <p className=" text-white">
+                    SEATS
+                  </p>
                   <label className="border p-1 flex flex-row w-44 h-10">
                     <input
                       type="number"
-                      placeholder="Seats"
                       name="people"
                       onChange={handleChange}
                       className="w-44"
@@ -114,21 +136,34 @@ export default (params) => {
                   </label>
                 </div>
                 <div className="col-start-1 col-end-4 row-start-3 row-end-3 p-5 flex flex-col ">
-                  <p className="flex justify-center">SPECIAL REQUESTS</p>
-                  <label className="border p-1 flex flex-row w-full">
-                    <input type="text" className='w-full' name="phone" onChange={handleChange} />
+                  <p className="flex justify-center text-white">
+                    SPECIAL REQUESTS
+                  </p>
+                  <label className="border p-1 flex flex-row w-full h-44">
+                    <input
+                      type="text"
+                      className="w-full"
+                      name="phone"
+                      onChange={handleChange}
+                    />
                   </label>
                 </div>
                 <input
-                  className="col-start-2 col-end-3 row-start-4 row-end-4 my-7 text-xs bg-black text-white"
+                  className="col-start-2 col-end-3 row-start-4 row-end-4 text-xs h-10 bg-black text-white"
                   type="submit"
                   value="BOOK NOW"
                 />
               </form>
-              </div>
-              
-          </div>) : null}
+            </div>
+          </div>
+        ) : null}
       </div>
+      <div
+        className={`${
+          modalIsOpen &&
+          "static z-30 bg-black h-screen w-screen opacity-50 transform -translate-y-12 translate-x-40"
+        }`}
+      ></div>
     </div>
   );
 };
