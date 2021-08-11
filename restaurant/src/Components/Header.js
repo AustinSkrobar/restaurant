0b142660db1764cc68532bab4ad982df505e65bb
Connor Yates <connoryates@Connors-MacBook-Pro.local>
2021-08-11 11:54:07 -0500

    title on header looks better

--- restaurant/src/Components/Header.js
+++ restaurant/src/Components/Header.js
@@ -4,7 +4,7 @@
 export default (params) => {
   return (
     <div className="static z-40 bg-scroll ">
-      <div className="absolute text-white top-60 left-60">
+      <div className="absolute text-white top-80 left-72">
         {" "}
         <h3 className="font-cursiveText text-4xl "> Welcome to</h3> 
         <h1 className="text-4xl bold">Grand Avenue</h1>{" "}
